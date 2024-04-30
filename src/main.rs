e71b91e3c23086804624388bc8de80b05379b5f5
lukas-heiligenbrunner <lukas.heiligenbrunner@gmail.com>
2024-04-30 11:04:00 +0200

    outsource to seperate functions

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
+use anyhow::anyhow;
 use ics::components::Property;
 use ics::{Event, ICalendar, Standard, TimeZone};
 use rocket::response::status::NotFound;
 use rocket::Config;
+use table_extract::Row;
 
 #[macro_use]
 extern crate rocket;
@@ -20,11 +22,11 @@ fn rocket() -> _ {
 
 #[get("/")]
 async fn index() -> Result<String, NotFound<String>> {
-    let ical = get_sheet().await;
+    let ical = create_calendar().await;
     return ical.map_err(|e| NotFound(format!("Error: {}", e.to_string())));
 }
 
-async fn get_sheet() -> Result<String, Box<dyn std::error::Error>> {
+async fn create_calendar() -> anyhow::Result<String> {
     let body = reqwest::get(URL).await?.text().await?;
 
     let mut calendar = ICalendar::new("2.0", "steinbock-kalender");
@@ -33,44 +35,58 @@ async fn get_sheet() -> Result<String, Box<dyn std::error::Error>> {
         Standard::new("19961027T030000", "+0100", "+0200"),
     ));
 
-    let table = table_extract::Table::find_first(body.as_str()).unwrap();
+    let table = table_extract::Table::find_first(body.as_str())
+        .ok_or(anyhow!("Failed to find table in html"))?;
+    // skip first because of table header
     for row in table.iter().skip(1) {
-        let date = row
-            .iter()
-            .as_slice()
-            .get(0)
-            .map_or_else(|| "Datum missing", |v| v.as_str());
-        let sec = row
-            .iter()
-            .as_slice()
-            .get(1)
-            .map_or_else(|| "sec missing", |v| v.as_str());
-        println!("{} at {} umgeschraubt", date, sec,);
+        let (date, section_name) = decode_table(row)?;
 
-        let section_name = sec.to_string();
+        let date_timestamp = create_timestamp(date);
+        let timestamp = format!("{}T124650Z", date_timestamp);
 
-        let dtstampdate = date
-            .rsplit(".")
-            .map(|x| x.to_string())
-            .collect::<Vec<String>>()
-            .join("");
-        let dtstamp = format!("{}T124650Z", dtstampdate);
-
-        let uid = format!("{}{}", dtstamp, section_name)
-            .split_whitespace()
-            .collect::<Vec<_>>()
-            .join("");
-        let mut event = Event::new(uid, dtstamp);
+        let uid = create_uid(section_name.clone(), timestamp.clone());
+        let mut event = Event::new(uid, timestamp);
 
         event.push(Property::new(
             "SUMMARY",
-            format!("Steinbock schraubt: {}", sec),
+            format!("Steinbock schraubt: {}", section_name),
         ));
-        event.push(Property::new("DTSTART;VALUE=DATE", dtstampdate.clone()));
-        event.push(Property::new("DTEND;VALUE=DATE", dtstampdate));
+        event.push(Property::new("DTSTART;VALUE=DATE", date_timestamp.clone()));
+        event.push(Property::new("DTEND;VALUE=DATE", date_timestamp));
 
         calendar.add_event(event);
     }
 
     Ok(calendar.to_string())
+}
+
+fn create_timestamp(date: String) -> String {
+    date.rsplit(".")
+        .map(|x| x.to_string())
+        .collect::<Vec<String>>()
+        .join("")
+}
+
+fn create_uid(section_name: String, timestamp: String) -> String {
+    let uid = format!("{}{}", timestamp, section_name)
+        .split_whitespace()
+        .collect::<Vec<_>>()
+        .join("");
+    uid
+}
+
+fn decode_table(row: Row) -> anyhow::Result<(String, String)> {
+    let date = row
+        .iter()
+        .as_slice()
+        .get(0)
+        .ok_or(anyhow!("Date not in table"))?;
+    let sec = row
+        .iter()
+        .as_slice()
+        .get(1)
+        .ok_or(anyhow!("Section not in table"))?;
+    println!("{} at {} umgeschraubt", date, sec,);
+
+    Ok((date.to_string(), sec.to_string()))
 }
